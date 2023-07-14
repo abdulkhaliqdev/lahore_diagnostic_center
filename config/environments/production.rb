61378eddb9fd3c111f1cc9939eda56f163976065
Abdul Khaliq <abdul.khaliq@eritheialabs.io>
2023-07-14 19:42:20 +0500

    Add compiler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,6 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
   
   # Do not fallback to assets pipeline if a precompiled asset is missed.

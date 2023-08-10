25addfe645a860aef1c61981ab508dce72e47214
abdulkhaliqdev <abdulkhaliqdev@gmail.com>
2023-08-10 06:29:08 +0500

    Add Timezone pakistan

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Again
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    config.time_zone = 'Islamabad'
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
